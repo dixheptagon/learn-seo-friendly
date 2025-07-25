b5009154bd623c5109e2420cc64a8195d5c6c876
dixheptagon <forentino006@gmai.com>
2025-07-25 15:47:18 +0700

    remove image link addres and add opengraph image

--- src/app/seo-friendly/metadata.ts
+++ src/app/seo-friendly/metadata.ts
@@ -10,8 +10,6 @@ export const metadata: Metadata = {
     title: "Purwadhika Digital School",
     description:
       " Bootcamp terpercaya sejak tahun 1987, Telah menghasilkan ratusan alumni yang bekerja di berbagai industri digital",
-    images:
-      "https://media.licdn.com/dms/image/v2/C560BAQH0yBAB8DAnkQ/company-logo_200_200/company-logo_200_200/0/1673600834208/purwadhika_logo?e=2147483647&v=beta&t=K6nDNdp45oDZPxjCMVn1aTIh65dWKbz6PJSkWdYrprY",
     url: "https://www.purwadhika.com",
     siteName: "Purwadhika School",
     locale: "id-ID",
