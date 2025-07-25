795c9e1a858421aad5f1e2fd9e05f45501bad0b6
dixheptagon <forentino006@gmai.com>
2025-07-25 15:43:09 +0700

    add new img link addres

--- src/app/seo-friendly/metadata.ts
+++ src/app/seo-friendly/metadata.ts
@@ -10,7 +10,8 @@ export const metadata: Metadata = {
     title: "Purwadhika Digital School",
     description:
       " Bootcamp terpercaya sejak tahun 1987, Telah menghasilkan ratusan alumni yang bekerja di berbagai industri digital",
-    images: "image/png",
+    images:
+      "https://media.licdn.com/dms/image/v2/C560BAQH0yBAB8DAnkQ/company-logo_200_200/company-logo_200_200/0/1673600834208/purwadhika_logo?e=2147483647&v=beta&t=K6nDNdp45oDZPxjCMVn1aTIh65dWKbz6PJSkWdYrprY",
     url: "https://www.purwadhika.com",
     siteName: "Purwadhika School",
     locale: "id-ID",
